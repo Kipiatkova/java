91e5ee2e515a114c65e82d19a4d2f040b8892c9a
Kipiatkova <kipiatkova1998@mail.ru>
2021-11-25 23:54:46 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 2000000000;
+        int moneyTrancfer = 500000000;
+        int all = balance + moneyTrancfer;
+        System.out.println(all);
+    }
+}
