a39efc005dc3bcbeac7244eebc782d18e0bc1e3c
Kipiatkova <kipiatkova1998@mail.ru>
2021-11-26 00:22:26 +0300

    Added md

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        int balance = 2000000000;
-        int moneyTrancfer = 500000000;
+        int balance = 2_000_000_000;
+        int moneyTrancfer = 500_000_000;
         int all = balance + moneyTrancfer;
         System.out.println(all);
     }
